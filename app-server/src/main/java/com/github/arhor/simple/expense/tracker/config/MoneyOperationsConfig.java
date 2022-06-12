d903b1d042a38af9628fafef8479aef86bcd5d9e
Maskim Buryshynets <v-mburyshynets@expediagroup.com>
2022-06-12 15:14:27 +0300

    javax.money scheduling configured;

--- app-server/src/main/java/com/github/arhor/simple/expense/tracker/config/MoneyOperationsConfig.java
+++ app-server/src/main/java/com/github/arhor/simple/expense/tracker/config/MoneyOperationsConfig.java
@@ -3,7 +3,6 @@ package com.github.arhor.simple.expense.tracker.config;
 import javax.money.convert.ExchangeRateProvider;
 import javax.money.convert.MonetaryConversions;
 
-import org.javamoney.moneta.convert.ExchangeRateType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +11,6 @@ public class MoneyOperationsConfig {
 
     @Bean
     public ExchangeRateProvider exchangeRateProvider() {
-        return MonetaryConversions.getExchangeRateProvider(
-            ExchangeRateType.ECB,
-            ExchangeRateType.ECB_HIST90,
-            ExchangeRateType.ECB_HIST
-        );
+        return MonetaryConversions.getExchangeRateProvider();
     }
 }
