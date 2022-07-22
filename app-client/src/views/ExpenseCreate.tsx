bf49c01c3a2597c1f02a69a0e6c89ccfff8fce6a
Maskim Buryshynets <v-mburyshynets@expediagroup.com>
2022-07-22 14:53:31 +0300

    parallel build disabled

--- app-client/src/views/ExpenseCreate.tsx
+++ app-client/src/views/ExpenseCreate.tsx
@@ -2,16 +2,10 @@ import { observer } from 'mobx-react';
 
 import Box from '@mui/material/Box';
 
-import { useStore } from '@/store';
-
-const ExpenseCreate = () => {
-    const { expense } = useStore();
-
-    return (
-        <Box>
-            {'create new expense'}
-        </Box>
-    );
-}
+const ExpenseCreate = () => (
+    <Box>
+        {'create new expense'}
+    </Box>
+);
 
 export default observer(ExpenseCreate);
