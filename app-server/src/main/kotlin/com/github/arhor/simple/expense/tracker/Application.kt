08afa896e44152d5552035b7ded8ed66f4324308
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-08-21 14:42:36 +0300

    completely decided - moving to kotlin =)

---
+++ app-server/src/main/kotlin/com/github/arhor/simple/expense/tracker/Application.kt
@@ -0,0 +1,32 @@
+package com.github.arhor.simple.expense.tracker;
+
+import com.github.arhor.simple.expense.tracker.config.properties.ApplicationProps
+import org.slf4j.LoggerFactory
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
+import java.lang.invoke.MethodHandles
+
+private val log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+
+@SpringBootApplication(proxyBeanMethods = false)
+@ConfigurationPropertiesScan(basePackageClasses = [ApplicationProps::class])
+class Application {
+
+    @Bean
+    fun applicationRunner(env: Environment) = ApplicationRunner {
+        val port = env.getProperty("server.port")
+        val path = env.getProperty("server.servlet.context-path", "/")
+
+        if (port != null) {
+            log.info("Local access URL: http://localhost:{}{}", port, path);
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
