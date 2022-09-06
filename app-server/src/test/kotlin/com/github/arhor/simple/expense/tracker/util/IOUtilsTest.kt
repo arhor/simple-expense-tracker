ac1e4bda6f2fecdfbd702ad007e1a690a72b86bd
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-09-06 17:45:29 +0300

    async IO improved;

--- app-server/src/test/kotlin/com/github/arhor/simple/expense/tracker/util/IOUtilsTest.kt
+++ app-server/src/test/kotlin/com/github/arhor/simple/expense/tracker/util/IOUtilsTest.kt
@@ -1,44 +1,28 @@
 package com.github.arhor.simple.expense.tracker.util
 
+import kotlinx.coroutines.flow.reduce
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.internal.bytebuddy.utility.RandomString
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
-import java.nio.ByteBuffer
-import java.nio.channels.AsynchronousFileChannel
 import java.nio.file.Path
 
-@Suppress("VARIABLE_WITH_REDUNDANT_INITIALIZER")
 internal class IOUtilsTest {
 
     @TempDir
     private lateinit var tempDir: Path
 
     @Test
-    fun `should asynchronously read file larger than allocated byte buffer using coroutine`() {
+    fun `should asynchronously read file larger than defined chunk size using coroutine`() {
         // given
-        val capacity = 16
+        val capacity = 256
 
         val text = RandomString.make(capacity * 5)
         val path = tempDir.resolve("test-file.txt").also { it.toFile().writeText(text) }
 
         // when
-        var result = ByteArray(0)
-
-        AsynchronousFileChannel.open(path).use { channel ->
-            val bytes = ByteBuffer.allocate(capacity)
-            val array = bytes.array()
-
-            runBlocking {
-                var num = 0
-                var pos = 0L
-                while (channel.readAsync(bytes, pos).also { num = it; pos += it } != -1) {
-                    result += array.copyOf(num)
-                    bytes.clear()
-                }
-            }
-        }
+        val result = runBlocking { path.chunkedFlow(chunkSize = capacity).reduce(ByteArray::plus) }
 
         // then
         assertThat(result)
