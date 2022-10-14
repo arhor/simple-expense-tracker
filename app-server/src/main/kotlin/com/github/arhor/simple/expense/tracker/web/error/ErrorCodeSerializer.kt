5079ff714ef7788af7b35d478c05e71bb2fcd494
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-10-14 04:55:03 -0400

    added docs to ErrorCodeSerializer;

--- app-server/src/main/kotlin/com/github/arhor/simple/expense/tracker/web/error/ErrorCodeSerializer.kt
+++ app-server/src/main/kotlin/com/github/arhor/simple/expense/tracker/web/error/ErrorCodeSerializer.kt
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
 
+/**
+ * Serializes [ErrorCode] instance to the string prefixed with corresponding [ErrorCode.type],
+ * also including error code numeric value with fixed length.
+ */
 class ErrorCodeSerializer : StdSerializer<ErrorCode>(ErrorCode::class.java) {
 
     override fun serialize(value: ErrorCode, generator: JsonGenerator, provider: SerializerProvider) {
