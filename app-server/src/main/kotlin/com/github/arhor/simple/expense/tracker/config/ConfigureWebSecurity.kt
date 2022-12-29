856c38056d840cbf102f9c12733d9a67fa2bf967
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2022-12-29 20:30:12 +0300

    fixed an issue with redundant API calls after successful firm sign in;

--- app-server/src/main/kotlin/com/github/arhor/simple/expense/tracker/config/ConfigureWebSecurity.kt
+++ app-server/src/main/kotlin/com/github/arhor/simple/expense/tracker/config/ConfigureWebSecurity.kt
@@ -13,7 +13,6 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.DefaultRedirectStrategy
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.AbstractAuthenticationTargetUrlRequestHandler
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler
 import org.springframework.web.util.UriComponentsBuilder
@@ -36,18 +35,18 @@ class ConfigureWebSecurity {
             }
             .logout {
                 it.logoutUrl(appProps.apiUrlPath(URL_PATH_SIGN_OUT))
-                it.logoutSuccessHandler(SimpleUrlLogoutSuccessHandler().also(::configure))
+                it.logoutSuccessHandler(SimpleUrlLogoutSuccessHandler().apply { setUseReferer(true) })
                 it.logoutSuccessUrl(URL_PATH_ROOT)
             }
             .formLogin {
                 it.loginPage(URL_PATH_SIGN_IN)
                 it.loginProcessingUrl(appProps.apiUrlPath(URL_PATH_SIGN_IN))
-                it.successHandler(SavedRequestAwareAuthenticationSuccessHandler().also(::configure))
+                it.successHandler(CustomSuccessHandler())
             }
             .oauth2Login {
                 it.loginPage(URL_PATH_SIGN_IN)
                 it.authorizationEndpoint().baseUri(appProps.authRequestBaseUri)
-                it.successHandler(CustomSuccessHandler(userService::createNewUserIfNecessary).also(::configure))
+                it.successHandler(CustomSuccessHandler(doBeforeRedirect = userService::createNewUserIfNecessary))
             }
         return http.build()
     }
@@ -55,43 +54,43 @@ class ConfigureWebSecurity {
     @Bean
     fun passwordEncoder() = BCryptPasswordEncoder()
 
-    private fun configure(handler: AbstractAuthenticationTargetUrlRequestHandler) {
-        handler.setUseReferer(true)
-    }
-
-    class CustomSuccessHandler(
-        private val doBeforeRedirect: (Authentication) -> Unit
-    ) : SavedRequestAwareAuthenticationSuccessHandler() {
-
-        init {
-            redirectStrategy = SuccessSignInRedirectStrategy
-        }
-
-        override fun onAuthenticationSuccess(req: HttpServletRequest, res: HttpServletResponse, auth: Authentication) {
-            doBeforeRedirect(auth)
-            super.onAuthenticationSuccess(req, res, auth)
-        }
-    }
-
-    object SuccessSignInRedirectStrategy : DefaultRedirectStrategy() {
-        override fun calculateRedirectUrl(contextPath: String?, url: String?): String {
-            return super.calculateRedirectUrl(contextPath, url).let { path ->
-                if (path.endsWith(URL_PATH_SIGN_IN)) {
-                    path.let(UriComponentsBuilder::fromUriString)
-                        .queryParam(PARAM_SUCCESS)
-                        .build()
-                        .toString()
-                } else {
-                    path
-                }
-            }
-        }
-    }
-
     companion object {
         private const val URL_PATH_ROOT = "/"
         private const val URL_PATH_SIGN_IN = "/sign-in"
         private const val URL_PATH_SIGN_OUT = "/sign-out"
         private const val PARAM_SUCCESS = "success"
+
+        private object SuccessSignInRedirectStrategy : DefaultRedirectStrategy() {
+            override fun calculateRedirectUrl(contextPath: String?, url: String?): String =
+                super.calculateRedirectUrl(contextPath, url).let { path ->
+                    if (path.endsWith(URL_PATH_SIGN_IN)) {
+                        path.let(UriComponentsBuilder::fromUriString)
+                            .queryParam(PARAM_SUCCESS)
+                            .build()
+                            .toString()
+                    } else {
+                        path
+                    }
+                }
+        }
+
+        private class CustomSuccessHandler(
+            private val doBeforeRedirect: (Authentication) -> Unit = {}
+        ) : SavedRequestAwareAuthenticationSuccessHandler() {
+
+            init {
+                setUseReferer(true)
+                redirectStrategy = SuccessSignInRedirectStrategy
+            }
+
+            override fun onAuthenticationSuccess(
+                request: HttpServletRequest,
+                response: HttpServletResponse,
+                authentication: Authentication
+            ) {
+                doBeforeRedirect(authentication)
+                super.onAuthenticationSuccess(request, response, authentication)
+            }
+        }
     }
 }
