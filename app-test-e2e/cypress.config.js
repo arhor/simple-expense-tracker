586b9f0806c6b6cc4f33ac44476adde3f987a3a7
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2023-02-24 15:46:24 +0300

    removed cypress module;

--- app-test-e2e/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-    video: false,
-    screenshotOnRunFailure: false,
-    e2e: {
-        baseUrl: 'http://localhost:5000',
-    },
-});
