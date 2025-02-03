9878868e74d79c0b811fd4b55fd76128a17f6a7e
Maksim Buryshynets <38556161+arhor@users.noreply.github.com>
2025-02-03 10:08:22 +0100

    Update Expense.tsx

--- apps/client/src/components/Expense/Expense.tsx
+++ apps/client/src/components/Expense/Expense.tsx
@@ -4,7 +4,7 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 
-import ExpenseIcon from '~/components/ExpenseIcon';
+import ExpenseIcon from '@/components/ExpenseIcon';
 
 // TODO: consider non-optional props consistent with model
 export type Props = {
